d0f60baf00f63ebd2045b42e54e7604c28868bf5
Kai-EV <81797227+Kai-EV@users.noreply.github.com>
2021-05-03 22:57:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -20,28 +20,28 @@ def simple_dice(dice_size, dice_num):
     m = dice_val
     return m
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
 
-    if message.content.startswith('dice'):
-        info = parse('dice {}d{}', message.content)
-        if info:
-            if info[1].isdecimal() and info[0].isdecimal():
-                dice_num = int(info[0])
-                dice_size = int(info[1])
-                #key = info[2]
-                # メッセージを書きます
-                #m = message.author.name + ' '
-                # if key == '一時的狂気':
-                #     m = temp_madness()
-                # elif key == '不定の狂気':
-                #     m = ind_madness()
-                #if key == 'dice':
-                m = simple_dice(dice_size, dice_num)
-                msg = 'dice: ' + str(np.sum(m)) + ' = ' + str(m)
-                await message.channel.send(msg)
+#     if message.content.startswith('dice'):
+#         info = parse('dice {}d{}', message.content)
+#         if info:
+#             if info[1].isdecimal() and info[0].isdecimal():
+#                 dice_num = int(info[0])
+#                 dice_size = int(info[1])
+#                 #key = info[2]
+#                 # メッセージを書きます
+#                 #m = message.author.name + ' '
+#                 # if key == '一時的狂気':
+#                 #     m = temp_madness()
+#                 # elif key == '不定の狂気':
+#                 #     m = ind_madness()
+#                 #if key == 'dice':
+#                 m = simple_dice(dice_size, dice_num)
+#                 msg = 'dice: ' + str(np.sum(m)) + ' = ' + str(m)
+#                 await message.channel.send(msg)
                 
                
 @bot.event
