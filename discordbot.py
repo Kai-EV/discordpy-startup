1bce016be06a3d3574b866f5243cd12e1d983e2f
Kai-EV <81797227+Kai-EV@users.noreply.github.com>
2021-05-03 23:30:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,6 +8,18 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
           
 
+def dice(dice_size):
+    num = np.random.randint(1, int(dice_size))
+    return num
+
+def simple_dice(dice_size, dice_num):
+    dice_val = np.array([], dtype=np.int64)
+    for i in range(dice_num):
+        dice_val = np.append(dice_val, dice(dice_size))
+    #msg = 'dice: ' + str(np.sum(dice_val)) + ' = ' + str(dice_val)
+    m = dice_val
+    return m
+
 
           
 
@@ -26,7 +38,6 @@ async def ping(ctx):
 @bot.command()
 async def neko(ctx):
     await ctx.send('nyan')
-
 
 
 bot.run(token)
