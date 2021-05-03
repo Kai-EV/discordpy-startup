c3165016d91a84c0b8fbe5de3b610fc85ea31461
Kai-EV <81797227+Kai-EV@users.noreply.github.com>
2021-05-04 00:51:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -60,11 +60,17 @@ token = os.environ['DISCORD_BOT_TOKEN']
 # async def neko(ctx):
 #     await ctx.send('nyan')
 
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+def dice(dice_size):
+    num = np.random.randint(1, int(dice_size))
+    return num
+
+def simple_dice(dice_size, dice_num):
+    dice_val = np.array([], dtype=np.int64)
+    for i in range(dice_num):
+        dice_val = np.append(dice_val, dice(dice_size))
+    #msg = 'dice: ' + str(np.sum(dice_val)) + ' = ' + str(dice_val)
+    m = dice_val
+    return m
 
 # メッセージ受信時に動作する処理
 @client.event
