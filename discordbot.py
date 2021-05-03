1fc506a68a7427233a6d3ab431d787b95f8d25f3
Kai-EV <81797227+Kai-EV@users.noreply.github.com>
2021-05-04 00:25:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,19 +23,19 @@ def simple_dice(dice_size, dice_num):
     return m
 
 
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == 'neko':
-        await message.channel.send('にゃーん')
+# @client.event
+# async def on_message(message):
+#     # メッセージ送信者がBotだった場合は無視する
+#     if message.author.bot:
+#         return
+#     # 「/neko」と発言したら「にゃーん」が返る処理
+#     if message.content == 'neko':
+#         await message.channel.send('にゃーん')
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('dice'):
-          await client.send_message(message.channel, 'bot')
+# @client.event
+# async def on_message(message):
+#     if message.content.startswith('dice'):
+#           await client.send_message(message.channel, 'bot')
           
 
 @bot.event
@@ -43,6 +43,26 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+
+
+@bot.command()
+async def dice(ctx):
+    info = parse('/dice {}d{}', message.content)
+        if info:
+            if info[1].isdecimal() and info[0].isdecimal():
+                dice_num = int(info[0])
+                dice_size = int(info[1])
+                #key = info[2]
+                # メッセージを書きます
+                #m = message.author.name + ' '
+                # if key == '一時的狂気':
+                #     m = temp_madness()
+                # elif key == '不定の狂気':
+                #     m = ind_madness()
+                #if key == 'dice':
+                m = simple_dice(dice_size, dice_num)
+                msg = 'dice: ' + str(np.sum(m)) + ' = ' + str(m)
+                await ctx.send(msg)
 
 
 @bot.command()
