a3b904e1750bd721b9b170f4be90695afdb26fbf
Kai-EV <81797227+Kai-EV@users.noreply.github.com>
2021-05-04 10:10:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -60,10 +60,10 @@ async def on_message(message):
                 msg = 'dice: ' + str(np.sum(m)) + ' = ' + str(m)
                 await message.channel.send(msg)
     
-#     if message.content = 'CCB':
-#           m = simple_dice(100, 1)
-#           msg = 'dice: ' + str(np.sum(m)) + ' = ' + str(m)
-#           await message.channel.send(msg)
+    if message.content == 'CCB':
+          m = simple_dice(100, 1)
+          msg = 'dice: ' + str(np.sum(m)) + ' = ' + str(m)
+          await message.channel.send(msg)
 
 
 client.run(token)
