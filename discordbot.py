e8205504e96dacfee6739cc2f0a7d594e1c9433b
Kai-EV <81797227+Kai-EV@users.noreply.github.com>
2021-05-04 11:46:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -70,7 +70,11 @@ async def on_message(message):
         if info:
             if info[0].isdecimal():
                 m = simple_dice(100, 1)
-                if int(m) <= int(info[0]):
+                if int(m) <= (int(info[0])/5):
+                    msg = 'dice: ' + str(np.sum(m)) + ' = ' + str(m) + ' <= ' + str(info[0]) + ' Extreme!!!'
+                elif (int(info[0])/5) < int(m) <= (int(info[0])/2):
+                    msg = 'dice: ' + str(np.sum(m)) + ' = ' + str(m) + ' <= ' + str(info[0]) + ' Hard!!'
+                elif (int(info[0])/2) < int(m) <= int(info[0]):
                     msg = 'dice: ' + str(np.sum(m)) + ' = ' + str(m) + ' <= ' + str(info[0]) + ' Succese!'
                 elif int(m) > int(info[0]):
                     msg = 'dice: ' + str(np.sum(m)) + ' = ' + str(m) + ' > ' + str(info[0]) + ' Failure.' 
