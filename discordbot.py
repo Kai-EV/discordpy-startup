6bc79b9a26dec7f7e2b5e68b5e59f6e167bcef55
Kai-EV <81797227+Kai-EV@users.noreply.github.com>
2021-05-06 13:33:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -133,8 +133,8 @@ async def on_message(message):
         roll.append('dice: [8] -> パニック')
         roll.append('dice: [9] -> 恐怖症の獲得（1d100をロールするかKPが1つ選ぶ')
         roll.append('dice: [0] -> マニアの獲得（1d100をロールするかKPが1つ選ぶ')
-        m = roll[simple_dice(10)]
-        a = simple_dice(10)
+        m = roll[simple_dice(10, 1)]
+        a = simple_dice(10, 1)
         m += '\ndice: ['+ str(a) +'] -> 一時的狂気(' + str(a) + '時間後に意識を取り戻す)'
         await message.channel.send(m)
 
