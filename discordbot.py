2cddafa241e5adbe883b1b3e12bfe0405d0bcae6
Kai-EV <81797227+Kai-EV@users.noreply.github.com>
2021-05-06 15:36:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -103,7 +103,7 @@ async def on_message(message):
                 await message.channel.send(msg)
     
     if message.content.startswith('/p'):
-        info = parse('/p{}CCB', message.content):
+        info = parse('/p{}CCB', message.content)
         if info:
             j = int(info[0])
             m = dice(10)
