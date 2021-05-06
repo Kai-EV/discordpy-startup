069718f43ba9b31c6fece045c000305b146e7745
Kai-EV <81797227+Kai-EV@users.noreply.github.com>
2021-05-06 14:09:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -113,8 +113,8 @@ async def on_message(message):
         roll.append('dice: [6] -> 失神')
         roll.append('dice: [7] -> パニックになって逃亡')
         roll.append('dice: [8] -> 身体的ヒステリーもしくは感情爆発')
-        roll.append('dice: [9] -> 恐怖症の獲得（1d100をロールするかKPが1つ選ぶ')
-        roll.append('dice: [0] -> マニアの獲得（1d100をロールするかKPが1つ選ぶ')
+        roll.append('dice: [9] -> 恐怖症の獲得（1d100をロールするかKPが1つ選ぶ)')
+        roll.append('dice: [0] -> マニアの獲得（1d100をロールするかKPが1つ選ぶ)')
         m = roll[dice(10)]
         a = dice(10)
         m += '\ndice: ['+ str(a) +'] -> 一時的狂気(' + str(a) + 'ラウンド) or (' + str(a) + '時間)'
@@ -131,8 +131,8 @@ async def on_message(message):
         roll.append('dice: [6] -> 重要な人々')
         roll.append('dice: [7] -> 収容')
         roll.append('dice: [8] -> パニック')
-        roll.append('dice: [9] -> 恐怖症の獲得（1d100をロールするかKPが1つ選ぶ')
-        roll.append('dice: [0] -> マニアの獲得（1d100をロールするかKPが1つ選ぶ')
+        roll.append('dice: [9] -> 恐怖症の獲得（1d100をロールするかKPが1つ選ぶ)')
+        roll.append('dice: [0] -> マニアの獲得（1d100をロールするかKPが1つ選ぶ)')
         m = roll[dice(10)]
         a = dice(10)
         m += '\ndice: ['+ str(a) +'] -> 一時的狂気(' + str(a) + '時間後に意識を取り戻す)'
